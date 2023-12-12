b8427bc79b2e6312e2f70f06705d53102e81e8f5
root <root@localhost>
2023-12-12 11:29:33 +0200

    add precentage count to free-mem.sh

--- .config/polybar/scripts/free-mem.sh
+++ .config/polybar/scripts/free-mem.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-free -h  |  grep Mem: | awk '{print $3"B"}' | awk '{print substr($0, 0, length($0)-3) " " substr($0, length($0)-2, length($0)) }'
+total_memory=$(free -h  |  grep Mem: | awk '{print $3"B"}' | awk '{print substr($0, 0, length($0)-3) " " substr($0, length($0)-2, length($0)) }')
+used_memory_percent=$(free | grep Mem | awk '{print int($3/$2 * 100.0)}' )
+
+echo $total_memory $used_memory_percent%
 
