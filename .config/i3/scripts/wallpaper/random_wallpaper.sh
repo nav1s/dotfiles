de7e830e35da41693a61c3375bc08d645b73243b
root <root@localhost>
2023-12-12 11:29:33 +0200

    small fixes

--- .config/i3/scripts/wallpaper/random_wallpaper.sh
+++ .config/i3/scripts/wallpaper/random_wallpaper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ln -f -s "$(find '/home/aviv/Pictures/Wallpapers/Abstract/' -type f | shuf -n 1)" ~/bg.jpg
+ln -f -s "$(find ~/Pictures/Wallpapers/Abstract/ -type f | shuf -n 1)" ~/bg.jpg
 feh --bg-fill ~/bg.jpg
 
