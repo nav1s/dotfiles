c463fe6d603dd159d0ab7bc0a257799c6b5bdd5d
root <root@localhost>
2023-12-12 11:29:33 +0200

    add tokyo-night-by-bitterhalt polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar i3 2>&1 | tee -a /tmp/polybar.log & disown
 
-# Launch Polybar
-MONITOR=HDMI-A-0 polybar i3 --config=~/.config/polybar/config.ini &
-
-MONITOR=eDP polybar i3 --reload --config=~/.config/polybar/config.ini &
-
+echo "Polybar launched..."
