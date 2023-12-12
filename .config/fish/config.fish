07f4442da89d8ca8e9565c70aa122ae269c04fa7
root <root@localhost>
2023-12-12 11:29:33 +0200

    move abbr to config.fish due to removal of universal abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,6 +36,63 @@ complete -c tldr -xa (tldr --list | sed 's/\W/ /g')
 # set ledger journal path
 set --global --export LEDGER_FILE "$HOME/Notes/finance/hledger/journal/current.journal"
 
+## Abbreviations ##
+
+# general
+abbr --add -- wget 'wget --hsts-file=/home/user/.local/share/wget-hsts'
+abbr --add -- l lsd
+abbr --add -- ll 'lsd -lah'
+abbr --add -- ls 'lsd -lah'
+abbr --add -- rm trash
+abbr --add -- ssh 'env TERM=xterm-256color ssh'
+abbr --add -- rwd 'cd (cat /tmp/termwd)'
+abbr --add -- ssh-keygen 'ssh-keygen -t ed25519'
+abbr --add -- cp 'cpg -g'
+abbr --add -- mv 'mvg -g'
+abbr --add -- lsb 'lsblk -f'
+
+# utils
+abbr --add -- c cht.sh
+abbr --add -- td tldr
+abbr --add -- radeontop 'radeontop --color'
+abbr --add -- swd 'pwd > /tmp/termwd'
+abbr --add -- hx helix
+
+# zfs
+abbr --add -- zlt "zfs list -t snapshot | rg --invert-match 'docker'"
+abbr --add -- zl "zfs list | rg --invert-match 'docker'"
+
+# docker
+abbr --add -- dc 'docker compose'
+abbr --add -- dco 'docker compose up -d'
+
+# hledger
+abbr --add -- h hledger
+abbr --add -- ha 'hledger iadd'
+
+# yadm
+abbr --add -- y yadm
+abbr --add -- ya 'yadm add'
+abbr --add -- ys 'yadm status'
+abbr --add -- yr 'yadm reset'
+abbr --add -- yap 'yadm add -p'
+abbr --add -- yp 'yadm push'
+abbr --add -- yc 'yadm commit -m'
+abbr --add -- yd 'yadm diff'
+
+# git
+abbr --add -- g git
+abbr --add -- gcl 'git clone'
+abbr --add -- gcm 'git commit -m'
+abbr --add -- ga 'git add'
+abbr --add -- gl 'git log'
+abbr --add -- gst 'git status'
+abbr --add -- gap 'git add -p'
+abbr --add -- gcd 'git clone --depth 1'
+abbr --add -- gr 'git reset'
+abbr --add -- gp 'git push'
+abbr --add -- gd 'git diff'
+
 # add extra funcs
 source ~/.config/fish/funcs.fish
 
