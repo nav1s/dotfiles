053fd594c20bc4656de092750fb1b2b28901e262
root <root@localhost>
2023-12-12 11:29:33 +0200

    improve notify messages and add sed filters for output

--- .config/i3/scripts/img2latex.fish
+++ .config/i3/scripts/img2latex.fish
@@ -1,9 +1,14 @@
 #!/usr/bin/fish
 set fname clipboard.png
-set path /tmp/
+set path /tmp
+
 flameshot gui
-xclip -selection clipboard -t image/png -o > $path$fname
+notify-send "Processing Image..."
+xclip -selection clipboard -t image/png -o > $path/$fname
 set output (pix2tex /tmp/clipboard.png)
-echo $output | xclip -selection clipboard
-notify-send "123"
+echo $output | 
+sed 's.\\operatorname\*{lim}.\\llim\\\limits.g' |
+sed 's.}}.} }.g' | 
+xclip -selection clipboard
+notify-send "Donezo"
 
