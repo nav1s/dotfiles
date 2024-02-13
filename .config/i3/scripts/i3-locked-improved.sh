b3552768225999e18b2fe8cf9ab08432daa7accd
root <root@localhost>
2024-02-13 21:09:15 +0200

    add i3 lock script in order to automatically switch to english when locking

---
+++ .config/i3/scripts/i3-locked-improved.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+setxkbmap -model pc104 -layout us -option grp:alt_shift_toggle
+playerctl --player=spotify pause
+i3lock --pass-media-keys --color 000000 --keylayout 2 
+setxkbmap -model pc104 -layout us,il -option grp:alt_shift_toggle
