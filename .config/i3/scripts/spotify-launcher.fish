69956dc99f4060166421e936ec1f68ff5724b87d
root <root@localhost>
2024-10-08 12:09:49 +0300

    add new spotify script to i3 conf

---
+++ .config/i3/scripts/spotify-launcher.fish
@@ -0,0 +1,8 @@
+#! /usr/bin/env fish
+
+env LD_PRELOAD=/usr/lib/spotify-adblock.so $HOME/.local/share/spotify-launcher/install/usr/bin/spotify &> /dev/null &
+~/.config/i3/scripts/output-switcher headphones
+openscq31-cli set ambient-sound-mode noise-canceling
+spd-say "battery precentage: $(bluetoothctl info | grep -i battery | awk '{print $NF}')"
+playerctl --player=spotify play-pause
+
