7f046ed0284148266dd864bb69218c3246ae10ea
root <root@localhost>
2025-09-26 15:03:42 +0300

    chore: trim battery precentage from spd-say in spotify-launcher.fish

--- .config/i3/scripts/spotify-launcher.fish
+++ .config/i3/scripts/spotify-launcher.fish
@@ -3,6 +3,6 @@
 env LD_PRELOAD=/usr/lib/spotify-adblock.so $HOME/.local/share/spotify-launcher/install/usr/bin/spotify &> /dev/null &
 ~/.config/i3/scripts/output-switcher headphones
 openscq31-cli set ambient-sound-mode noise-canceling
-spd-say "battery precentage: $(bluetoothctl info | grep -i battery | awk '{print $NF}')"
+spd-say "$(bluetoothctl info | grep -i battery | awk '{print $NF}')"
 playerctl --player=spotify play-pause
 
