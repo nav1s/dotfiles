5144db71e491b8330340609ee526c277a8de5cb0
nav1s <42621369+nav1s@users.noreply.github.com>
2026-08-01 08:03:47 +0300

    chore: change play pause to work for mpv

--- .config/i3/scripts/spotify-launcher.fish
+++ .config/i3/scripts/spotify-launcher.fish
@@ -1,10 +1,9 @@
 #! /usr/bin/env fish
 
-env LD_PRELOAD=/usr/lib/spotify-adblock.so $HOME/.local/share/spotify-launcher/install/usr/bin/spotify &> /dev/null &
+env LD_PRELOAD=/usr/lib/spotify-adblock.so $HOME/.local/share/spotify-launcher/install/usr/bin/spotify &>/dev/null &
 ~/.config/i3/scripts/output-switcher headphones
 openscq31-cli set ambient-sound-mode noise-canceling
 spd-say "$(bluetoothctl info | grep -i battery | awk '{print $NF}')"
-playerctl --player=spotify play-pause
+playerctl --player=mpv play-pause
 # playerctl --player=vlc play-pause
 # playerctl --player=firefox.instance_1_61 play-pause
-
